6be98e264d627975c84f67c8a52ae9c280422936
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-04-09 14:49:24 -0400

    Building Infrastructure

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Navbar />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/search" component={Search} />
+          </Wrapper>
+          <Footer />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
