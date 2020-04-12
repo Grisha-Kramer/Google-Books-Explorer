b66a878aff82b67fc3b7812d8c77ea3f0169302c
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-04-12 17:08:46 -0400

    Importing class project

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
-import Footer from "./components/Footer";
-import Search from "./pages/Search";
-import Navbar from "./components/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navbar />
-          <Wrapper>
-            <Route exact path="/" component={Search} />
-            <Route exact path="/saved" component={Saved} />
-            <Route exact path="/search" component={Search} />
-          </Wrapper>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/books"]}>
+            <Books />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
